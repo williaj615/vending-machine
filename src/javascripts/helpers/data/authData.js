28d4b2fba23cb752709c61804a6eadbe362beed3
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-11-02 10:37:54 -0500

    corrected auth functions

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,12 +1,25 @@
 import firebase from'firebase/app';
 import 'firebase/auth';
+import $ from 'jquery';
+
+const logoutButton = $('#navbar-button-logout');
+const loginButton = $('#auth');
+const loginContent = $('#stock');
+
+
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
         // someone is logged in-- we should not see auth component
+        logoutButton.removeClass('hide');
+        loginButton.addClass('hide');
+        loginContent.removeClass('hide');
     } else {
         // nobody logged in-- SHOW auth component
+        logoutButton.addClass('hide');
+        loginButton.removeClass('hide');
+        loginContent.addClass('hide');
     }
   })
 };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,14 +2,14 @@ import 'bootstrap';
 import '../styles/main.scss';
 import firebase from 'firebase';
 import apiKeys from './helpers/apiKeys.json';
-import loginButton from './components/Auth/auth';
+import login from './components/Auth/auth';
 import authData from './helpers/data/authData';
 import myNavbar from './components/myNavbar/myNavbar';
 
 const init = () => {
     firebase.initializeApp(apiKeys.firebaseKeys);
     authData.checkLoginStatus();
-    auth.loginButton();
+    login.loginButton();
     myNavbar.logoutEvent();
 };
 
