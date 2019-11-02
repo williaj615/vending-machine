8b53d187747a17f82c3c6872b8d3a420c150acfd
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-11-02 13:43:05 -0500

    snacks ordered properly

--- src/javascripts/components/machine/machine.js
+++ src/javascripts/components/machine/machine.js
@@ -12,6 +12,7 @@ const buildTheMachine = () => {
     // 3. use MachineId to get all snack positions --DONE
     // 4. use uid of snackpositions/positions to get available snacks for that machine --DONE
     // 5. SMASH em - return an array of positions (in order A1, A2, A3, B1...)
+    // so positions should have position.snack if a snack exists at that position.
 }
 
 export default { buildTheMachine };
--- src/javascripts/helpers/data/positionData.js
+++ src/javascripts/helpers/data/positionData.js
@@ -9,12 +9,14 @@ const getAllPositionsByMachineId = (machineId) => new Promise((resolve, reject) 
         const demPositions = response.data;
         const positions = [];
         Object.keys(demPositions).forEach((fbId) => {
-        demPositions[fbId].id = fbId;
-        positions.push(demPositions[fbId]);
+            demPositions[fbId].id = fbId;
+            positions.push(demPositions[fbId]);
         });
-        resolve(positions); 
+        // order positions A1, A2, A3, B1, B2, B3, C1, C2, C3
+        const sortedPositions = positions.sort((a, b) => a.position.localeCompare(b.position, 'en', { numeric: true }));
+        resolve(sortedPositions); 
     })
     .catch((error) => reject(error));
-})
+});
 
 export default { getAllPositionsByMachineId }
